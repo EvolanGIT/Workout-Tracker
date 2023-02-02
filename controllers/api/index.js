a87b2adcfdf25c05502ce4c1ba1b02236948ccc1
Evolan <yon_baermund@hotmail.com>
2023-02-02 11:06:44 -0500

    made comments all around so we can find parts more efficiently

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
+// Imports, mostly all the variables up top will be imports required for the site to work properly. depending on the file, different imports are required.
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require("./postRoutes")
 
+// this specifices to the router which api route to follow.
 router.use('/users', userRoutes);
 router.use("/posts", postRoutes);
 
