b54da094de8e1b2b2fa03f9b21eb54648625a3e2
Evolan <yon_baermund@hotmail.com>
2023-02-06 17:47:42 -0500

    footer done, added the codebruhs image to the images folder. view should be compelte now.

--- public/js/post.js
+++ public/js/post.js
@@ -2,21 +2,18 @@ const postFormHandler = async (event) => {
   
   event.preventDefault();
 
-  console.log('this button works')
-    // Gather the data from the form elements on the page
-    const userName= document.querySelector('#profile')
+  console.log('this button works');
     const post_title = document.querySelector('#post-title').value.trim();
     //placeholder for updated wt variable
     const post_text = document.querySelector('#post-text').value.trim();
 
-
-    
     if (post_title && post_text) {
       const response = await fetch("/api/posts", {
         method: "POST",
         body: JSON.stringify({ post_title, post_text}),
         headers: { "Content-Type": "application/json" },
-      });
+      })
+      
 
       if (response.ok) {
         console.log("post successful")
@@ -30,5 +27,6 @@ const postFormHandler = async (event) => {
 //  $('#myModal').on('shown.bs.modal', function () {
 //   $('#myInput').trigger('focus')
 // })
-  document.querySelector("#postForm")
-  .addEventListener("submit", postFormHandler)
+
+
+  document.querySelector("#postForm").addEventListener("submit", postFormHandler);
